db160df2f0441545df4c711851b7b1f809efce65
Mostafa Saeed <https://github.com/Desha-Saeed>
2022-05-04 08:01:20 +0200

    finished design except for dropdowns

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    
+    extend: {
+      colors: {
+        mediumGray:"hsl(0, 0%, 41%)",
+        almostBlack: "hsl(0, 0%, 8%)",
+        almostWhite: "hsl(0, 0%, 98%)"
+      },
+    },
+  },
+  plugins: [],
+}
